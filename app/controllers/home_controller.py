85c54510e17b6b458d705ce833081de2ed9e4a0b
juanchacon2021 <chaconjuan439@gmail.com>
2026-05-04 22:00:37 -0400

    feat: Actualizar archivos .pyc y limpiar comentarios en index.html

--- app/controllers/home_controller.py
+++ app/controllers/home_controller.py
@@ -6,4 +6,4 @@ home_blueprint = Blueprint('home', __name__)
 @home_blueprint.route('/')
 def home():
     # Aquí el controlador decide qué vista mostrar
-    return render_template('index.html', mensaje="¡Hola desde MVC!")
+    return render_template('index.html')
